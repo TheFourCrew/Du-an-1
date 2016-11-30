fa33264d9b0ab2ec58df76e0472c39964ffd580e
NguyenMinh\MinhNguyen <minhncpk00629@fpt.edu.vn>
2016-11-30 18:27:44 +0700

    Cập nhật Servlet LienLac

--- Project 1/src/java/com/javaweb/controller/LienLac.java
+++ Project 1/src/java/com/javaweb/controller/LienLac.java
@@ -51,7 +51,7 @@ public class LienLac extends HttpServlet {
         try {
             Email.sendEmail(host, port, user, pass, recipient,  csubject, cMessage);
             resultMessage += "The e-mail was sent successfully";
-            getServletContext().getRequestDispatcher("/lienlac.jsp").forward(request, response);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
         } catch (Exception e) {
             resultMessage += "There was an error: " + e.getMessage();
             getServletContext().getRequestDispatcher("/lienlac.jsp").forward(request, response);
