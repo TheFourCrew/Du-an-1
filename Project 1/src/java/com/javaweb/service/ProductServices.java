a1bf7dd393ccf0e577cdeaacedc5fb859a6b0440
NguyenMinh\MinhNguyen <minhncpk00629@fpt.edu.vn>
2016-11-30 17:03:10 +0700

    Thêm hàm insert sản phẩm

--- Project 1/src/java/com/javaweb/service/ProductServices.java
+++ Project 1/src/java/com/javaweb/service/ProductServices.java
@@ -1,9 +1,33 @@
 package com.javaweb.service;
 
+import com.javaweb.hibernate.util.HibernateUtil;
+import com.javaweb.model.Product;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author MinhNguyen
  */
 public class ProductServices {
-    
+
+    public boolean InsertOrUpdateProduct(Product sp) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            session.saveOrUpdate(sp);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+        } finally {
+            session.close();
+        }
+        return false;
+    }
 }
