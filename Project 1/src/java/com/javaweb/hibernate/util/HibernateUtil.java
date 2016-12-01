09ca5922265a06933c293889aa04c450dfc680ae
tovantu <togiadapda4@gmail.com>
2016-12-01 15:39:42 +0700

    Sua hibernate

--- Project 1/src/java/com/javaweb/hibernate/util/HibernateUtil.java
+++ Project 1/src/java/com/javaweb/hibernate/util/HibernateUtil.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.javaweb.hibernate.util;
 
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -7,7 +12,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author MinhNguyen
+ * @author Admin
  */
 public class HibernateUtil {
 
