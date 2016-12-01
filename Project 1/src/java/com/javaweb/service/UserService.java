fd9e8c157482adae2eb7b486c35ebffde517c3c6
tovantu <togiadapda4@gmail.com>
2016-12-01 15:39:29 +0700

    Them Userservice

--- Project 1/src/java/com/javaweb/service/UserService.java
+++ Project 1/src/java/com/javaweb/service/UserService.java
@@ -7,6 +7,7 @@ package com.javaweb.service;
 
 import com.javaweb.hibernate.util.HibernateUtil;
 import com.javaweb.model.User;
+import java.util.ArrayList;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -52,4 +53,65 @@ public class UserService {
         }
         return user;
         }
+     public User getUserByID(String userID){
+        Session session =HibernateUtil.getSessionFactory().openSession();
+        Transaction tx=null;
+        try {
+            tx=session.getTransaction();
+            tx.begin();
+            String strQuery="from User where iduser="+userID;
+            Query query=session.createQuery(strQuery);
+            User user=(User)query.uniqueResult();
+            tx.commit();
+            return user;
+        } catch (Exception e) {
+            if(tx!=null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return null;
+    }
+     public boolean InserUser(User user){
+        Session session= HibernateUtil.getSessionFactory().openSession();
+        Transaction tx= null;
+        try {
+            
+            tx=session.getTransaction();
+            tx.begin();
+            session.saveOrUpdate(user);
+            tx.commit();
+            return true;
+        } catch (Exception e) {
+            if(tx!=null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return false;
+    }
+     public ArrayList<User>GetAllUsers(){
+        Session session =HibernateUtil.getSessionFactory().openSession();
+        Transaction tx=null;
+        ArrayList<User>listUsers=new ArrayList<User>();
+        try {
+            tx=session.getTransaction();
+            tx.begin();
+            Query query=session.createQuery("from User");
+            listUsers=(ArrayList) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if(tx!=null){
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        }finally{
+            session.close();
+        }
+        return listUsers;
+    }
 }
