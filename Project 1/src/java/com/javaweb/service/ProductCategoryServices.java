4e9795b828e6fdd6497f8b99d44daa2a0ff1842f
NguyenMinh\MinhNguyen <minhncpk00629@fpt.edu.vn>
2016-12-01 22:01:51 +0700

    Đổi tên ProductCategoryServices

---
+++ Project 1/src/java/com/javaweb/service/ProductCategoryServices.java
@@ -0,0 +1,36 @@
+package com.javaweb.service;
+
+import com.javaweb.hibernate.util.HibernateUtil;
+import java.util.ArrayList;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author MinhNguyen
+ */
+public class ProductCategoryServices {
+
+    public ArrayList<ProductCategoryServices> getAll() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tx = null;
+        ArrayList<ProductCategoryServices> aPC = null;
+        try {
+            tx = session.getTransaction();
+            tx.begin();
+            String strQuery = "from ProductCategory";
+            Query query = session.createQuery(strQuery);
+            aPC = (ArrayList<ProductCategoryServices>) query.list();
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            System.out.println(e.toString());
+        } finally {
+            session.close();
+        }
+        return aPC;
+    }
+}
