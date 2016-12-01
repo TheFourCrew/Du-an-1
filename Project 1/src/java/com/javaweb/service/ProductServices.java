37c1907b79315a10a92d2c55af9baa9549c910dc
NguyenMinh\MinhNguyen <minhncpk00629@fpt.edu.vn>
2016-12-01 18:03:37 +0700

    Thêm hàm ghi ảnh

--- Project 1/src/java/com/javaweb/service/ProductServices.java
+++ Project 1/src/java/com/javaweb/service/ProductServices.java
@@ -2,6 +2,10 @@ package com.javaweb.service;
 
 import com.javaweb.hibernate.util.HibernateUtil;
 import com.javaweb.model.Product;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import org.apache.commons.fileupload.FileItemStream;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -27,6 +31,33 @@ public class ProductServices {
             }
         } finally {
             session.close();
+        }
+        return false;
+    }
+    
+    //Hàm thực hiện ghi file vào thư mục có sẵn
+    public static boolean processFile(String path, FileItemStream item) {
+        try {
+            File f = new File(path + File.separator + "imageUpload");
+            if (!f.exists()) {
+                f.mkdir();
+            }
+            File savedFile = new File(f.getAbsolutePath() + File.separator + item.getName());
+            FileOutputStream fos = new FileOutputStream(savedFile);
+            InputStream is = item.openStream();
+
+            int x = 0;
+            byte[] b = new byte[1024];
+
+            while ((x = is.read(b)) != -1) {
+                fos.write(b, 0, x);
+            }
+
+            fos.flush();
+            fos.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e.toString());
         }
         return false;
     }
